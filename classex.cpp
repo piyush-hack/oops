cffbf4d1301db8778a1a68aa6b2ab3fe34011670
Piyush Puniya <75742731+piyush-hack@users.noreply.github.com>
2022-05-22 14:10:10 +0530

    first commit

---
+++ classex.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class student
+{
+public:
+    int id;
+    // data member
+    int mobile;
+    string name;
+
+    int add(int x, int y)
+    {
+        // member functions
+        return x + y;
+    }
+};
+
+int main(){
+
+   return 0;
+}
